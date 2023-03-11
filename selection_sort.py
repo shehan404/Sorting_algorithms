156a557e27a434236963006edd3915208eb622dc
Shehan <shehanon1@gmail.com>
2023-03-11 20:59:20 +0530

    added code for selection sort and added selection sort gif to images folder

---
+++ selection_sort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+
+
+    for i in range(len(arr)-1):
+        min_index=i
+        for j in range(i,len(arr)):
+            if arr[j]<arr[min_index]:
+                min_index=j
+
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+
+    return arr
+
+to_sort = [2,3,6,1,10,7,9,4,0,5]
+
+print(selection_sort(to_sort))
