e3c026c8fb079ea8f4791c741bff717fa006e2ae
Shehan <shehanon1@gmail.com>
2023-03-11 15:07:18 +0530

    added the bubble sort function in bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,13 @@
-print("bubble sort")
-print("master")
-print("bbb")
+#this function implement the bubble sort
+def bubble_sort(arr):
+    for i in range(0,len(arr)-1):
+        for j in range(0,len(arr)-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+#enter the array which want to sort here
+to_sort=[4,5,6,4,6,8,4,2,1]
+
+print(bubble_sort(to_sort))
+
